ce91038a695cab4079429ad94d3862c088497570
Joey Wan <joeyhongjiwan@gmail.com>
2024-01-05 21:34:50 -0500

    improved check function

--- functions.py
+++ functions.py
@@ -1,15 +1,16 @@
 import re
 import requests
+from PIL import Image
+import cv2 as cv
 
-def image_check(path: str)-> tuple:
-    """
-    Input: path to image file
-    Function determines if the input is a valid image file
-    """
-    if re.search("(?i)\.(jpg|png|webp|bmp|dib|jpeg|jpg|jpe|jp2|pbm|pgm|ppm|pxm|pnm|sr|ras|tiff|tif|exr|hdr|pic)$",path):
-        pass
-    else:
+def image_check(path: str)-> list:
+    if re.search("(?i)\.(jpg|png|webp|bmp|dib|jpeg|jpg|jpe|jp2|pbm|pgm|ppm|pxm|pnm|sr|ras|tiff|tif|exr|hdr|pic)$",path) == False:
         raise ValueError("Input valid image file.")
-    
+    with Image.open(path) as file:
+        width, heigth = file.size
+    image = cv.imread(path)
+    cv.namedWindow('Image', cv.WINDOW_NORMAL)
+    cv.resizeWindow('Image',width,heigth)
+    return cv.selectROI("Image",image)
 
     
